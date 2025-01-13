5f54183fb1facc34c68bc405f68dabfc543cb523
mohan7603 <146535223+mohan7603@users.noreply.github.com>
2025-01-13 07:25:01 -0800

    Add files via upload

---
+++ Widering.java
@@ -0,0 +1,13 @@
+public class Widering{
+    public static void main(String args[]){
+        int MyInt =10;
+        double MyDouble = MyInt;  // Automatically casting Int to Double ;
+
+        System.out.println("Before Converting to Type Casting");
+        System.out.println(MyInt);
+        
+
+        System.out.println("After Converting to Type Casting");
+        System.out.println(MyDouble);
+    }
+}
