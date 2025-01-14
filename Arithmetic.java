0e32b62e6877b56b1e1228853c5a01b13f576d54
Mohan <146535223+mohan7603@users.noreply.github.com>
2025-01-14 06:21:58 -0800

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+// ARITHMETIC OPERATOR
+
+public class Arithmetic{
+    public static void main(String args[])
+    {
+        // Create variables
+
+        int a = 30;
+        int b = 20;
+         
+         //Addition
+        System.out.println("Addition       :"+(a+b));    // OUTPUT : 50
+         //Subraction
+        System.out.println("Subraction     :"+(a-b));   // OUTPUT : 10
+        //Multiplication
+        System.out.println("Multiplication :"+(a*b));   // OUTPUT : 600
+         //Division
+        System.out.println("Division       :"+(a/b));   // OUTPUT : 1
+         //Modules
+        System.out.println("Modules        :"+(a%b));   // OUTPUT : 10
+         
+    }
+}
---
+++ Assignment.java
@@ -0,0 +1,17 @@
+// ASSIGNMENT OPERATOR
+
+public class Assignment{
+    public static void main(String args[]){
+
+        // create variables
+
+        int a = 10;
+        int b = 5;
+
+        a+=5; // a=a+5 (a=10+5)
+        b-=3; // b=b-5 (b=5-3)
+
+        System.out.println("A is "+a);  // OUTPUT : 15
+        System.out.println("B is "+b);  // OUTPUT : 2
+    }
+}
