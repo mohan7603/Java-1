470e3f0f3f18ea39bf51026a0e57bb564609f57f
Mohan <146535223+mohan7603@users.noreply.github.com>
2025-01-14 19:35:53 +0530

    Update Widering.java

--- Widering.java
+++ Widering.java
@@ -1,10 +1,12 @@
+// WIDERING CASTING(AUTOMATIC)
+
 public class Widering{
     public static void main(String args[]){
         int MyInt =10;
         double MyDouble = MyInt;  // Automatic casting Int to Double ;
 
         System.out.println("Before Converting to Type Casting");
-        System.out.println(MyInt);
+        System.out.println(MyInt);            
         
 
         System.out.println("After Converting to Type Casting");
