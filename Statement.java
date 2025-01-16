e6fb32f54a180192d65bae5131ae66e43143a7a5
Mohan <146535223+mohan7603@users.noreply.github.com>
2025-01-16 06:15:50 -0800

    Add files via upload

---
+++ Statement.java
@@ -0,0 +1,22 @@
+// If, Elseif Statements:
+
+public class Statement{
+    public static void main(String args[]){
+
+        String City = "Chennai";
+
+        if(City == "Mumbai"){
+            System.out.println("City Is Mumbai");
+        }
+        else if(City == "Bangalore"){
+            System.out.println("City Is Bangalore");
+        } 
+        else if(City == "Chennai"){
+            System.out.println("City Is Chennai");
+        } 
+        else{
+            System.out.println(City);
+        }
+
+    }
+}
