38de45589e371d42fcbb7114f96797951ead3943
Mohan <146535223+mohan7603@users.noreply.github.com>
2025-01-16 06:18:19 -0800

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,35 @@
+// RELATIONAL OPERATOR
+
+
+public class Relational{
+    public static void main(String args[]){
+
+        // create variables
+
+        int a = 30;
+        int b = 20;
+
+        // Value of a and b
+    System.out.println("a is "+a+" "+"and b is"+" "+b);
+
+       // == Equal operator
+    System.out.println(a==b);                  // OUTPUT : False
+
+      // != Not Equal to operator
+    System.out.println(a!=b);                  // OUTPUT : True
+
+      // > Greather than operator
+    System.out.println(a>b);                  // OUTPUT : True
+
+     // < less than operator
+    System.out.println(a<b);                  // OUTPUT : False
+
+      // >= Greather than or Equal to operator
+    System.out.println(a>=b);                 // OUTPUT : True
+
+      // <= Lesser than or Equal to  operator
+    System.out.println(a<=b);                 // OUTPUT : False 
+
+
+    }
+}
