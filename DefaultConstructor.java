56c062f3d6850269ad4dc4a2c52faa858996e989
Mohan <146535223+mohan7603@users.noreply.github.com>
2025-02-04 07:53:43 -0800

    Add files via upload

---
+++ DefaultConstructor.java
@@ -0,0 +1,11 @@
+class DefaultConstructor{
+
+    DefaultConstructor()
+    {
+        System.out.println("This is DefaultConstructor");
+    }
+    public static void main(String[]args)
+    {
+        DefaultConstructor d =new DefaultConstructor();
+    }
+}
